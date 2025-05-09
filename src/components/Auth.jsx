1eb5e31b838991bac00f2e4201212ae5bbe3b494
Zephyrior <mark.temblique@gmail.com>
2025-05-09 11:22:21 +0200

    funzionalità registrazione - ok

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -5,6 +5,9 @@ import api from "../services/api";
 const Auth = ({ token, setToken }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [nome, setNome] = useState("");
+  const [cognome, setCognome] = useState("");
+  const [email, setEmail] = useState("");
   const [isLogin, setIsLogin] = useState(true);
   const navigate = useNavigate();
 
@@ -14,9 +17,9 @@ const Auth = ({ token, setToken }) => {
     console.log("api url", apiUrl);
     try {
       const endpoint = isLogin ? "/api/auth/login" : "/api/auth/register";
-      const response = await api.post(`${apiUrl}${endpoint}`, { username, password });
+      const response = await api.post(`${apiUrl}${endpoint}`, isLogin ? { username, password } : { username, password, nome, cognome, email });
       localStorage.setItem("token", response.data.token);
-      setToken(response.data.token);
+      isLogin ? setToken(response.data.token) : localStorage.removeItem("token");
       navigate("/clienti");
     } catch (error) {
       alert(error.response?.data?.message || "Errore");
@@ -41,6 +44,19 @@ const Auth = ({ token, setToken }) => {
         <div className="mb-3">
           <input type="password" className="form-control" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
         </div>
+        {!isLogin && (
+          <div>
+            <div className="mb-3">
+              <input type="text" className="form-control" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} required />
+            </div>
+            <div className="mb-3">
+              <input type="text" className="form-control" placeholder="Cognome" value={cognome} onChange={(e) => setCognome(e.target.value)} required />
+            </div>
+            <div className="mb-3">
+              <input type="email" className="form-control" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            </div>
+          </div>
+        )}
         <button type="submit" className="btn btn-primary">
           {isLogin ? "Login" : "Registrati"}
         </button>
